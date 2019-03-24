9dce5b5f00110dbd3aa4a3472f9d8fc171e205b5
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-03-24 19:52:01 +0200

    Add crude command for running test on cursor

--- extension.js
+++ extension.js
@@ -2,8 +2,31 @@ const vscode = require("vscode");
 
 let taskProvider;
 
-function activate() {
+function activate(context) {
     console.debug('Activating just-testing...');
+
+    let disposable = vscode.commands.registerCommand('extension.runOnCursor', () => {
+        const activeLineNumber = vscode.window.activeTextEditor.selection.active.line;
+        let lineNumber = activeLineNumber;
+        let line;
+        while (lineNumber >= 0) {
+            line = vscode.window.activeTextEditor.document.lineAt(lineNumber).text;
+            if (line.includes("def test_")) break;
+            lineNumber--;
+        }
+
+        const fileName = vscode.workspace.asRelativePath(vscode.window.activeTextEditor.document.fileName);
+        const testName = line.match(/def (test_.+)\(/)[1];
+
+        if (vscode.window.terminals.length === 0) {
+            vscode.window.createTerminal("Just Testing");
+        }
+        const terminal = vscode.window.terminals[0]
+        terminal.show(true);
+        terminal.sendText(`python -m pytest ${fileName}::${testName}`);
+    });
+
+    context.subscriptions.push(disposable);
 
     taskProvider = vscode.tasks.registerTaskProvider(
         'pytest',
@@ -11,15 +34,15 @@ function activate() {
             provideTasks: () => {
                 return [
                     new vscode.Task(
-                        { type: 'test-all' },
-                        'test-all',
+                        { type: 'testAll' },
+                        'testAll',
                         'pytest',
                         new vscode.ShellExecution('python -m pytest -v'),
                         []
                     ),
                     new vscode.Task(
-                        { type: 'test-file' },
-                        'test-file',
+                        { type: 'testFile' },
+                        'testFile',
                         'pytest',
                         new vscode.ShellExecution('python -m pytest -v ${file}'),
                         []
