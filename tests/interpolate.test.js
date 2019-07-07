bf0a7cf907abc252ac3f3814c0a13ba4cc24c3f4
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-07-07 14:05:46 +0300

    Use a code snippet in tests

--- tests/interpolate.test.js
+++ tests/interpolate.test.js
@@ -1,26 +1,32 @@
-const { Interpolator } = require('../src/interpolate') // TODO GZL interpolator
+const { Interpolator } = require('../src/interpolate')
 
 describe('Interpolator', () => {
+  const lines = [
+    'def test_foo():',
+    '    assert True',
+    'def bar_test():',
+    '    assert True'
+  ]
+
   const helpers = {
     getSetting: (key) => {
       switch (key) {
         case 'baseCommand': return 'base-command'
-        case 'runOnCursorRegex': return '(line-3)'
+        case 'runOnCursorRegex': return 'def (test_.+)\\('
         case 'expressions': return {}
       }
     },
     getActiveEditor: () => ({
       document: {
         fileName: 'foo/bar/baz.py',
-        lineAt: (i) => ({ text: `line-${i}` })
+        lineAt: (i) => ({ text: lines[i] })
       },
-      selection: { active: { line: 5 } }
+      selection: { active: { line: 3 } }
     }),
     asRelativePath: (path) => path
   }
 
-  const interpolator = new Interpolator(helpers)
-  const interpolate = (template) => interpolator.interpolate(template)
+  const interpolate = (template) => (new Interpolator(helpers)).interpolate(template)
 
   it('interpolates base', () => {
     expect(interpolate('{base}')).toEqual('base-command')
@@ -35,11 +41,11 @@ describe('Interpolator', () => {
   })
 
   it('interpolates testName', () => {
-    expect(interpolate('{testName}')).toEqual('line-3')
+    expect(interpolate('{testName}')).toEqual('test_foo')
   })
 
   it('interpolates line', () => {
-    expect(interpolate('{line}')).toEqual('6')
+    expect(interpolate('{line}')).toEqual('4')
   })
 
   describe('expressions', () => {
@@ -48,23 +54,22 @@ describe('Interpolator', () => {
       getSetting: (key) => {
         switch (key) {
           case 'expressions': return {
-            valueExpression: { value: 'foo' },
-            regexExpression: { regex: '(line-2)' }
+            valueExpression: { value: 'static-value' },
+            regexExpression: { regex: 'def (.+_test)' }
           }
           default: return helpers.getSetting(key)
         }
       }
     }
 
-    const interpolator = new Interpolator(helpersWithExpressions)
-    const interpolate = (template) => interpolator.interpolate(template)
+    const interpolate = (template) => (new Interpolator(helpersWithExpressions)).interpolate(template)
 
     it('interpolates static expressions', () => {
-      expect(interpolate('{valueExpression}')).toEqual('foo')
+      expect(interpolate('{valueExpression}')).toEqual('static-value')
     })
 
     it('interpolates regex expressions', () => {
-      expect(interpolate('{regexExpression}')).toEqual('line-2')
+      expect(interpolate('{regexExpression}')).toEqual('bar_test')
     })
   })
 })
