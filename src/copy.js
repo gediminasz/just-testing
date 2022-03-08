cd386da79aa54d632363ff297d92fdcc842a2649
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2022-03-09 00:18:00 +0200

    Fix indentation

--- src/copy.js
+++ src/copy.js
@@ -4,20 +4,20 @@ const { interpolate, InterpolationError } = require('./interpolate')
 const helpers = require('./helpers')
 
 class CopyOnCursorCommand {
-    run () {
-        try {
-            const template = helpers.getSetting('runOnCursorCommand')
-            const command = interpolate(template)
-            vscode.env.clipboard.writeText(command)
-        } catch (e) {
-            if (e instanceof InterpolationError) {
-                vscode.window.showErrorMessage(e.message)
-            } else {
-                console.error(e)
-                throw e
-            }
-        }
+  run () {
+    try {
+      const template = helpers.getSetting('runOnCursorCommand')
+      const command = interpolate(template)
+      vscode.env.clipboard.writeText(command)
+    } catch (e) {
+      if (e instanceof InterpolationError) {
+        vscode.window.showErrorMessage(e.message)
+      } else {
+        console.error(e)
+        throw e
+      }
     }
+  }
 }
 
 module.exports = { CopyOnCursorCommand }
